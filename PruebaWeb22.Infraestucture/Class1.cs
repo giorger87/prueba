326f2c8cc98d33536e4f193dc57ec7f04e2647ef
gior piojoso <gior.87.lp@hotmail.com>
2018-07-06 20:33:47 -0300

    Confirmado

---
+++ PruebaWeb22.Infraestucture/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaWeb22.Infraestucture
+{
+    public class Class1
+    {
+    }
+}
